7c6fd177ba6002cb223491b110d0c49a5e4c658a
marijashakleva <maria.sakleva@gmail.com>
2019-08-04 19:14:27 +0200

    section 2 progress

--- custom.js
+++ custom.js
@@ -13,11 +13,13 @@ document.getElementById('slide2').addEventListener('click', function(){
     tl.to('.hero-title-one', 1.5, { opacity: 0,ease: Circ.easeOut, y: -1000 }, "-=1.85");
     tl.to('.hero-title-two', 1.5, { opacity: 0, ease: Circ.easeOut, x: -1000 }, "-=1.85");
     document.getElementById("slide2").classList.add('animate-how');
-    tl.to('#slide2', .5, { scale: 4, opacity: 1, ease: Power4.easeIn, x: 120},  "-=1.95");
-    tl.to('#slide2', .5, { opacity: 0, ease: Power4.easeIn});
+    tl.to('#slide2', .5, { scale: 4, opacity: 1, ease: Power4.easeIn},  "-=1.95");
+    tl.to('#slide2', .35, { opacity: 0, ease: Power4.easeIn});
     tl.set(".wrap", {opacity: 0, display:"none"});
     tl.set(".img-wrapper", {opacity: 0, display:"none"});
-    tl.set(".quote", {opacity: 1, display:"flex"}, "-=1");
-    tl.set("#one", {opacity: 0, delay: 8, display:"none"}, "-=1");
+    tl.set(".quote", {opacity: 1, display:"flex"});
+    tl.set("#first", {opacity: 0, delay: 8, display:"none"}, "-=.7");
+    // second slide start
+    // tl.set("#second", {opacity: 1, delay: 8, display:"flex"}, "-=.1");
 });
  
